3a37508aba5e71fb3b8da15dbc2f1bcefd3635d9
Mike Atkins <mike.atkins@lanetix.com>
2017-03-30 11:58:22 -0700

    Hello world

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'env',
+              'react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
